ac10818c00dc638c1efeeab7d98d202193741325
Kousthub Ganugapati <kousthub.ganugapati.738@k12.friscoisd.org>
2024-04-11 19:45:11 -0500

    finished the bar chart nobody touch it

---
+++ src/BarChart.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+const BarChart = () => {
+  useEffect(() => {
+    ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+  }, []);
+
+  const data = {
+    labels: ['Amazon', 'Facebook', 'Netflix', 'Google'],
+    datasets: [{
+      label: 'Stock Prices (USD)',
+      data: [189.05, 523.16, 628.78, 160.79], // Removed extra data points
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'category', // Specify x-axis scale type as 'category'
+      },
+    },
+  };
+
+  return (
+    <div style = {{ width: '50%' , float: 'right', margin: '0 auto', flexDirection: 'row', marginTop: 250}}>
+      <Bar data={data} options={options} />
+    </div>
+  );
+};
+
+export default BarChart;
